529a3cdcad186c7598035f7013f9e23a778f7600
deepraj_chouhan <deepraj.chouhan@iflair.com>
2025-04-11 19:19:19 +0530

    push lala

--- src/Database/Model/Marketing/ClientRequest.ts
+++ src/Database/Model/Marketing/ClientRequest.ts
@@ -1,3 +1,21 @@
 import mongoose, { Schema, Document } from "mongoose";
 
-interface I
+interface IClientsRequestDataType {
+  name: string,
+  lastname: string,
+  brandName: string,
+  email: string,
+  number: string,
+  message: string
+}
+
+const clientRequestData = new Schema<IClientsRequestDataType>({
+
+})
+
+const ClientRequestModel = mongoose.model<IClientsRequestDataType>(
+  "ClientRequest",
+  clientRequestData
+);
+
+export default ClientRequestModel;
