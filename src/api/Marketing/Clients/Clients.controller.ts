9910221c84274683ffd329614d53ef331626935d
deepraj_chouhan <deepraj.chouhan@iflair.com>
2025-04-15 15:39:51 +0530

    commit dist

--- src/api/Marketing/Clients/Clients.controller.ts
+++ src/api/Marketing/Clients/Clients.controller.ts
@@ -10,10 +10,11 @@ export class ClientController {
             console.log("");
             
             const storedClient = await clientRepositary.storeClient(requestData);
+            
             if(storedClient) {
-                res.status(config.statusCode.internalServer).json({message: "CLIENT_ADDED"})
+                res.status(config.statusCode.successful).json({message: "CLIENT_ADDED", success: 1})
             } else {
-                res.status(config.statusCode.internalServer).json({ error: "FAILED_TO_CREATE" });
+                res.status(config.statusCode.internalServer).json({ error: "FAILED_TO_CREATE", success: 0});
             }
         } catch (error: any) {
             res.status(config.statusCode.internalServer).json({ error: error.message });
